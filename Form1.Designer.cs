d980f2a0e2ea360e7fa1b366dfbd46cfc02a02fb
Kaylesse <kayless123@gmail.com>
2019-04-01 12:19:10 +0200

    Version 0.1

---
+++ Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace WoloSummonerIDApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.API_Key = new System.Windows.Forms.TextBox();
+            this.btnAPI = new System.Windows.Forms.Button();
+            this.lblVerify = new System.Windows.Forms.Label();
+            this.cbMultinick = new System.Windows.Forms.CheckBox();
+            this.txtNicks = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lblNick = new System.Windows.Forms.Label();
+            this.cbpassword = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // API_Key
+            // 
+            this.API_Key.Location = new System.Drawing.Point(63, 13);
+            this.API_Key.Name = "API_Key";
+            this.API_Key.Size = new System.Drawing.Size(231, 20);
+            this.API_Key.TabIndex = 0;
+            this.API_Key.TextChanged += new System.EventHandler(this.API_Key_TextChanged);
+            // 
+            // btnAPI
+            // 
+            this.btnAPI.Location = new System.Drawing.Point(300, 12);
+            this.btnAPI.Name = "btnAPI";
+            this.btnAPI.Size = new System.Drawing.Size(75, 23);
+            this.btnAPI.TabIndex = 1;
+            this.btnAPI.Text = "Check";
+            this.btnAPI.UseVisualStyleBackColor = true;
+            this.btnAPI.Click += new System.EventHandler(this.btnAPI_Click);
+            // 
+            // lblVerify
+            // 
+            this.lblVerify.AutoSize = true;
+            this.lblVerify.Location = new System.Drawing.Point(381, 17);
+            this.lblVerify.Name = "lblVerify";
+            this.lblVerify.Size = new System.Drawing.Size(27, 13);
+            this.lblVerify.TabIndex = 2;
+            this.lblVerify.Text = "N/A";
+            // 
+            // cbMultinick
+            // 
+            this.cbMultinick.AutoSize = true;
+            this.cbMultinick.Location = new System.Drawing.Point(13, 40);
+            this.cbMultinick.Name = "cbMultinick";
+            this.cbMultinick.Size = new System.Drawing.Size(104, 17);
+            this.cbMultinick.TabIndex = 3;
+            this.cbMultinick.Text = "More nicknames";
+            this.cbMultinick.UseVisualStyleBackColor = true;
+            this.cbMultinick.CheckedChanged += new System.EventHandler(this.cbMultinick_CheckedChanged);
+            // 
+            // txtNicks
+            // 
+            this.txtNicks.Location = new System.Drawing.Point(13, 64);
+            this.txtNicks.Name = "txtNicks";
+            this.txtNicks.Size = new System.Drawing.Size(250, 20);
+            this.txtNicks.TabIndex = 4;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(300, 40);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 5;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "API Key";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(382, 40);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 7;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // lblNick
+            // 
+            this.lblNick.AutoSize = true;
+            this.lblNick.Location = new System.Drawing.Point(297, 71);
+            this.lblNick.Name = "lblNick";
+            this.lblNick.Size = new System.Drawing.Size(27, 13);
+            this.lblNick.TabIndex = 8;
+            this.lblNick.Text = "N/A";
+            // 
+            // cbpassword
+            // 
+            this.cbpassword.AutoSize = true;
+            this.cbpassword.Location = new System.Drawing.Point(180, 40);
+            this.cbpassword.Name = "cbpassword";
+            this.cbpassword.Size = new System.Drawing.Size(94, 17);
+            this.cbpassword.TabIndex = 9;
+            this.cbpassword.Text = "Show API Key";
+            this.cbpassword.UseVisualStyleBackColor = true;
+            this.cbpassword.CheckedChanged += new System.EventHandler(this.cbpassword_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 96);
+            this.Controls.Add(this.cbpassword);
+            this.Controls.Add(this.lblNick);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.txtNicks);
+            this.Controls.Add(this.cbMultinick);
+            this.Controls.Add(this.lblVerify);
+            this.Controls.Add(this.btnAPI);
+            this.Controls.Add(this.API_Key);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "AccountID Getter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox API_Key;
+        private System.Windows.Forms.Button btnAPI;
+        private System.Windows.Forms.Label lblVerify;
+        private System.Windows.Forms.CheckBox cbMultinick;
+        private System.Windows.Forms.TextBox txtNicks;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label lblNick;
+        private System.Windows.Forms.CheckBox cbpassword;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Web;
+using System.Net;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+
+namespace WoloSummonerIDApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public String none = "N/A";
+
+        void Form1_Load(object sender, EventArgs e)
+        {
+            btnRun.Enabled = false;
+            lblNick.Text = none;
+            API_Key.UseSystemPasswordChar = true;
+        }
+
+        void cbMultinick_CheckedChanged(object sender, EventArgs e)
+        {
+            int iChecked = cbMultinick.Checked ? 1 : 0;
+            if (iChecked == 1)
+            {
+                this.Height = 615;
+                txtNicks.Multiline = true;
+                txtNicks.Height = 500;
+                txtNicks.Width = 250;
+                txtNicks.ScrollBars = ScrollBars.Vertical;
+                lblNick.Text = "";
+            }
+            else
+            {
+                this.Height = 135;
+                txtNicks.ScrollBars = ScrollBars.None;
+                txtNicks.Multiline = false;
+                lblNick.Text = none;
+            }
+        }
+
+        private void btnAPI_Click(object sender, EventArgs e)
+        {
+            String URL = "https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Kaylesse?api_key="+API_Key.Text;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+            var code = HttpStatusCode.Unauthorized;
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                code = response.StatusCode;
+            }
+            catch (Exception ex)
+            {
+                code = HttpStatusCode.Unauthorized;
+                lblVerify.Text = "Wrong";
+                lblVerify.ForeColor = Color.FromArgb(255, 0, 0);
+                MessageBox.Show(ex.Message, "API Key Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            if(code == HttpStatusCode.OK)
+            {
+                lblVerify.Text = "Correct";
+                lblVerify.ForeColor = Color.FromArgb(0, 255, 0);
+                btnRun.Enabled = true;
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        String getaccID(String s)
+        {
+            String URL1 = "https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + s + "?api_key=" + API_Key.Text;
+            HttpWebRequest request1 = (HttpWebRequest)WebRequest.Create(URL1);
+            try
+            {
+                HttpWebResponse response1 = (HttpWebResponse)request1.GetResponse();
+                Stream receiveStream1 = response1.GetResponseStream();
+                StreamReader readStream1 = new StreamReader(receiveStream1, Encoding.Default);
+                String data1 = readStream1.ReadToEnd();
+                var enID = JObject.Parse(data1)["accountId"].ToString();
+                String URL2 = "https://eun1.api.riotgames.com/lol/match/v4/matchlists/by-account/" + enID + "?api_key=" + API_Key.Text;
+                HttpWebRequest request2 = (HttpWebRequest)WebRequest.Create(URL2);
+                HttpWebResponse response2 = (HttpWebResponse)request2.GetResponse();
+                Stream receiveStream2 = response2.GetResponseStream();
+                StreamReader readStream2 = new StreamReader(receiveStream2, Encoding.Default);
+                String data2 = readStream2.ReadToEnd();
+                var gameID = JObject.Parse(data2)["matches"][0]["gameId"].ToString();
+                String URL3 = "https://eun1.api.riotgames.com/lol/match/v4/matches/" + gameID + "?api_key=" + API_Key.Text;
+                HttpWebRequest request3 = (HttpWebRequest)WebRequest.Create(URL3);
+                HttpWebResponse response3 = (HttpWebResponse)request3.GetResponse();
+                Stream receiveStream3 = response3.GetResponseStream();
+                StreamReader readStream3 = new StreamReader(receiveStream3, Encoding.Default);
+                String data3 = readStream3.ReadToEnd();
+                var temp = (JObject)JsonConvert.DeserializeObject(data3);
+                var ilegraczy = temp["participantIdentities"].Count();
+                String accHistoryLink = "";
+                for (int i = 0; i < ilegraczy; i++)
+                {
+                    if (temp["participantIdentities"][i]["player"]["summonerName"].ToString() == s)
+                    {
+                        accHistoryLink = temp["participantIdentities"][i]["player"]["matchHistoryUri"].ToString();
+                    }
+                }
+                String reversedaccHistoryLink = Reverse(accHistoryLink);
+                String reversedaccID = "";
+                foreach (char c in reversedaccHistoryLink)
+                {
+                    if (c == '/')
+                    {
+                        break;
+                    }
+                    reversedaccID += c;
+                }
+                none = Reverse(reversedaccID);
+                reversedaccID = "";
+                return none;
+            }
+            catch (Exception exc)
+            {
+                if (s == "")
+                {
+                    return "Empty line :>";
+                }
+                else
+                {
+                    return "Summoner doesn't exist";
+                }
+            }
+            
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            List<String> nicki = new List<string>();
+            Dictionary<String, String> lista = new Dictionary<string, string>();
+            lblNick.Text = "";
+            if(!cbMultinick.Checked)
+            {
+                String str = txtNicks.Text;
+                String temp = getaccID(str);
+                lblNick.Text = temp;
+            }
+            else
+            {
+                int ileLinii = txtNicks.Lines.Count();
+                for(int i=0;i<ileLinii;i++)
+                {
+                    nicki.Add(txtNicks.Lines[i]);
+                }
+                foreach(String item in nicki)
+                {
+                    String temp = getaccID(item);
+                    Thread.Sleep(3700);
+                    if(lista.ContainsKey(item))
+                    {
+                        continue;
+                    }
+                    lista.Add(item, temp);
+                }
+                using (StreamWriter writetext = new StreamWriter("lista.txt"))
+                {
+                    foreach(var item in lista)
+                    {
+                        writetext.WriteLine(item.Key + "\t" + item.Value + "\n");
+                    }
+                }
+                lblNick.Text = "Done";
+            }
+            nicki = null;
+            lista = null;
+        }
+
+        private void cbpassword_CheckedChanged(object sender, EventArgs e)
+        {
+            int iCheckedP = cbpassword.Checked ? 1 : 0;
+            if (iCheckedP == 0)
+            {
+                API_Key.UseSystemPasswordChar = true;
+            }
+            else
+            {
+                API_Key.UseSystemPasswordChar = false;
+            }
+        }
+
+        void API_Key_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
